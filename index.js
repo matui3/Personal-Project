17c99c84fffaecd768b7bc39b4e61cb80b4cbea1
matui3 <jonformantes@gmail.com>
2023-01-02 22:47:37 -0800

    updated slideshow

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 function imgChange() {
-    obj.style.backgroundImage = 'url(resources/FateZero.jpg)';
+    document.image.backgroundImage = images[i];
+
+    if (i < images.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }
+
+    setTimeout("imgChange()", time)
 }
 
-let obj = document.getElementById("backimg");
-const backimg = document.getElementById("img-change");
-backimg.addEventListener('click', () => imgChange());
+
+function showSlides(n) {
+    let i = 0;
+    let slides = document.getElementsByClassName("slide-default");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 5000);
+}
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// const backimg = document.getElementById("img-change");
+// backimg.addEventListener('click', () => imgChange());
